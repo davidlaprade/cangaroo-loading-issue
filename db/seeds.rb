81e5b4ba8f9f713c820198639f22e1e4be37f542
David Laprade <david.laprade@gmail.com>
2018-01-25 14:31:30 -0500

    Create a seed file with dummy connection info, run w/ `rake db:seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Cangaroo::Connection.where(
+  name:  'cangaroo_endpoint',
+  url:   'http://localhost:3100',
+  key:   'cangaroo',
+  token: 'cangaroo_token'
+).first_or_create
